1bcfceb799255e8357dd2534b1ff83dfdf624bf5
Ren22 <ngm.renat@gmail.com>
2017-06-01 02:54:44 +0200

    1try_hm7

---
+++ src/dz7_test.js
@@ -0,0 +1,6 @@
+var date = new Date;
+
+
+var ss = date.toGMTString();
+console.log(typeof(ss));
+console.log(ss);
--- src/index.js
+++ src/index.js
@@ -1,58 +1,58 @@
-/* ДЗ 6.1 - Асинхронность и работа с сетью */
+/* ДЗ 7.1 - BOM */
 
 /**
- * Функция должна создавать Promise, который должен быть resolved через seconds секунду после создания
+ * Функция должна создавать окно с указанным именем и размерами
  *
- * @param {number} seconds - количество секунд, через которое Promise должен быть resolved
- * @return {Promise}
+ * @param {number} name - имя окна
+ * @param {number} width - ширина окна
+ * @param {number} height - высота окна
+ * @return {Window}
  */
-function delayPromise(seconds = 1000) {
-    var promise = new Promise((resolve, reject) => {
+function createWindow(name, width, height) {
+    window.open(name, width, height);
 
-        setTimeout(() => {
-            resolve(1);
-        }, 1000); // если задать 1000 явно то сработает
-    });
-
-    return promise
+    return window;
 }
 
 /**
- * Функция должна вернуть Promise, который должен быть разрешен массивом городов, загруженным из
- * https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json
- * Элементы полученного массива должны быть отсортированы по имени города
+ * Функция должна закрывать указанное окно
  *
- * @return {Promise<Array<{name: String}>>}
+ * @param {Window} window - окно, размер которого надо изменить
  */
-function loadAndSortTowns() {
-    var promise = new Promise((resolve, reject) => {
-        var req = new XMLHttpRequest();
+function closeWindow(window) {
 
-        req.open('GET', 'https://raw.githubusercontent.com/smelukov/citiesTest/master/cities.json', true)
-        req.responseType = 'json';
-        req.onload = () => {
-            var res = req.response;
+    window.close();
+}
 
-            res.sort(function (a, b) { //является ли эта ф-ия callback ?
-                if (a.name > b.name) {
-                    return 1;
-                }
-                if (a.name < b.name) {
-                    return -1;
-                }
-                // a должно быть равным b
-                return 0;
-            });
+/**
+ * Функция должна создавать cookie с указанными именем и значением
+ *
+ * @param name - имя
+ * @param value - значение
+ */
+function createCookie(name, value) {
+    document.cookie = name + '=' + value;
 
-            resolve(res);
-        };
-        req.send();
-    });
+}
 
-    return promise;
+/**
+ * Функция должна удалять cookie с указанным именем
+ *
+ * @param name - имя
+ */
+function deleteCookie(name) {
+    var date = new Date();
+
+    document.cookie =
+        name +
+        '=;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+    //альтернатива, которая не сработала
+    //; expires = ' + date.setSeconds(date.getSeconds() - 1);
 }
 
 export {
-    delayPromise,
-    loadAndSortTowns
+    createWindow,
+    closeWindow,
+    createCookie,
+    deleteCookie
 };
